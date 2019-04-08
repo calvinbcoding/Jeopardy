9ca87bd5ea43e4b0b6d818af6b1f9284d734ee85
Calvin Bruch <calvinbcoding@gmail.com>
2019-04-08 17:29:44 -0600

    "MVP psuedo code added from an earlier typed file"

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+// // 1. Create a grid:
+// //  with -three columns and 
+// //       -6 rows (5 rows for $100-$500, and a header row).
+
+// 2. the header row should have 3 columns: HTML, CSS, JS
+// 3. the columns themselves should be $100-$500 (based on dificulty)
+//     -row 1 = headers 
+//     -row 2 = $100 questions
+//     -row 3 = $200 questions
+//     -row 4 = $300 questions
+//     -row 5 = $400 questions
+//     -row 6 = $500 questions
+// 4. when a player chooses a category and row, clicking on that square should reveal a 
+// question with t/f or multiple choice answers built into the square -(this could also 
+//     be a prompt), or a new screen entirely
+
+// 5. a second click should reveal either on one of the multiple choice answers or on a t/f 
+// prompt should award the correct amount of $ to the player
+
+// 6. a. a correct answer will add the $ amount to the player's score and remove the 
+//         question from the grid/table
+//     b. an incorrect answer will: 1.subtract the amounT OF $ the question was worth from
+//                                     the players' $/score
+//                         ### extra 2. an incorrect answer will move onto the next player
+
+// 7.
